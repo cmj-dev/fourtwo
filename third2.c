bacd74501182b1db5fbc6d7ca83d9c246a8e2a6c
cmj-dev <louis9982.dev@gmail.com>
2022-01-26 21:13:26 +0900

    third two

---
+++ third2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+	float a = 3.141592f;
+	int b = 12345;
+	double c = 3.141592f;
+
+	printf("a is %.3f\n",a);
+	printf("b is %5d\n",b);
+	printf("c is %6.3f\n",c);}
